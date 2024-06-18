87291b1a4ec016c1da0bb4f58c7c39a9c0d97869
EmberaK <vlasukula11@gmail.com>
2024-06-19 00:04:44 +0300

    added lesson_3

---
+++ lesson_3/lesson_3.1/style.js
@@ -0,0 +1,32 @@
+// Задача
+// Створити репозиторій із файлом index.html.
+// Додати туди невеликий скрипт, мета якого – вивести всі відомі вам типи даних у консоль.
+// Використовуючи оператор typeof та console.log
+
+console.log(typeof "Hello, world!");
+
+//number
+console.log(typeof 30);
+console.log(typeof 90);
+
+//BigInt (or big number)
+console.log(typeof BigInt(28283873834738947348387483947387489337384747));
+console.log(typeof BigInt(7847489743843434345435435435439804804));
+
+//string
+console.log(typeof "Anna");
+console.log(typeof "Yulia");
+
+//boolean
+console.log(typeof false);
+console.log(typeof true);
+
+//null
+console.log(typeof null);
+
+//undefined
+console.log(typeof undefined);
+
+//symbol
+console.log(typeof Symbol('bird'));
+
