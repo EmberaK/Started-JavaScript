efe14f4816f6c4dcc0270dfe743141285e845e81
EmberaK <vlasukula11@gmail.com>
2024-06-26 17:23:05 +0300

    added some details for lesson_5.4

--- lesson_5/lesson_5.3/script.js
+++ lesson_5/lesson_5.3/script.js
@@ -1,6 +1,6 @@
 let N = parseInt(prompt("Please enter an integer N:"));
 if (isNaN(N)) {
-  console.log("Invalid input. Please enter a valid integer.");
+  prompt("Invalid input. Please enter a valid integer.");
 } else {
   console.log(`All integers from 1 to 100 whose square does not exceed ${N} are:`);
   for (let i = 1; i <= 100; i++) {
--- lesson_5/lesson_5.4/script.js
+++ lesson_5/lesson_5.4/script.js
@@ -1,7 +1,7 @@
 let N = prompt("Please, write a integer and positive number");
 
 if (isNaN(N) || N <= 0) {
-    console.log("Please, write a integer and positive number");
+    N = prompt("Please, write a integer and positive number");
 } else {
     N = Number(N);
     function isPrime(num) {
