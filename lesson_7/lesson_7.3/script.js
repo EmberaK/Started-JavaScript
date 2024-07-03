1fc91e09bbcb427209859830d9e801206c847185
EmberaK <vlasukula11@gmail.com>
2024-07-03 17:49:01 +0300

    added some details for lesson_7.3

--- lesson_7/lesson_7.3/script.js
+++ lesson_7/lesson_7.3/script.js
@@ -1,16 +1,14 @@
-function boxInput(){
+function boxInput() {
     let inputUser;
-    for(let i = 0; i <=10; i++){
+    for (let i = 0; i <= 10; i++) {
         inputUser = prompt(`Please enter a number greater than 100`);
-        const againInputUser = inputUser;
-        if (Number(inputUser)>100 || String(inputUser)){
+        if (Number(inputUser) > 100 || isNaN(inputUser) ) {
             alert(`Result, last information: ${inputUser}`);
-        }
-        if (Number(inputUser)<100){
-            alert(`Please enter a number greater than 100`);
-            againInputUser;
+            break;
+        } else {
+            alert('Please enter a number greater than 100');
         }
     }
-    
+
 }
 boxInput();
