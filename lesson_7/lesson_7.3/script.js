d57cf7b46a27e56efbd7b9c199d86dde56e2d3e1
EmberaK <vlasukula11@gmail.com>
2024-07-06 10:25:13 +0300

    📄 docs: update script.js

--- lesson_7/lesson_7.3/script.js
+++ lesson_7/lesson_7.3/script.js
@@ -1,14 +1,14 @@
+
 function boxInput() {
-    let inputUser;
-    for (let i = 0; i <= 10; i++) {
-        inputUser = prompt(`Please enter a number greater than 100`);
-        if (Number(inputUser) > 100 || isNaN(inputUser) ) {
-            alert(`Result, last information: ${inputUser}`);
-            break;
+    let inputUser; // Объявляем переменную для хранения ввода пользователя
+    for (let i = 0; i <= 10; i++) { // Начинаем цикл, который выполнится максимум 11 раз
+        inputUser = prompt(`Please enter a number greater than 100`); // Запрашиваем у пользователя число больше 100
+        if (Number(inputUser) > 100 || isNaN(inputUser)) { // Проверяем, что введено число больше 100 или не число
+            alert(`Result, last information: ${inputUser}`); // Выводим результат, если условие выполнено
+            break; // Выходим из цикла
         } else {
-            alert('Please enter a number greater than 100');
+            alert('Please enter a number greater than 100'); // Просим ввести число больше 100, если введено меньше
         }
     }
-
 }
-boxInput();
+boxInput(); // Вызываем функцию
