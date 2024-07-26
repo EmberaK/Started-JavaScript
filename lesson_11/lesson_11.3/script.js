3953ea03911d3b41dbadafc88a9408e07f64a8f1
EmberaK <vlasukula11@gmail.com>
2024-07-26 11:24:02 +0300

    added some commits for lesson_11.3

---
+++ lesson_11/lesson_11.3/script.js
@@ -0,0 +1,14 @@
+// сделал из картинок масив
+const images =  ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"];
+// с помощью math рандомно выбираеться картинка 
+// random -  может и выдать доробовые числа(а индекс не может быть дробовым)
+// поэтому используем floor
+const randomImageIndex = Math.floor(Math.random() * images.length);
+// показываем какая картинка выпала
+const randomImage = images[randomImageIndex];
+// путь к картинке для  src, хотя можно бы было это не писать а сразу в строке 12
+// написать src="images/${randomImage}"
+const imagePath = `images/${randomImage}`;
+
+document.body.innerHTML = `<img src="${imagePath}" alt="Random Image">`;
+
