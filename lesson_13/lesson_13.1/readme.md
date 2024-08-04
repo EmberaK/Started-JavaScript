005d07287b3722bbfd3f31c50dd39603f509429d
EmberaK <vlasukula11@gmail.com>
2024-08-04 22:35:59 +0300

    added solution for lesson_13.1

---
+++ lesson_13/lesson_13.1/readme.md
@@ -0,0 +1,25 @@
+Task on ukr:
+Доробити валідацію для надсилання повідомлення з використанням регулярних виразів:
+
+Поля:
+
+- Name - обовьязкове текстове поле
+- Message - текстове поле не меньше 5 символів
+- Phone number - обовьязкове поле типу phone. З початком на +380
+- Email - email обовьязково повинен мати @ та крапку
+- Після відправки, в консоль відображаємо дані, які ввів користувач.
+
+Під час помилки показувати її під полем.
+
+Task on eng:
+Complete the validation to reinforce the message using regular expressions:
+
+Fields:
+
+- Name is a mandatory text field
+- Message - a text field of at least 5 characters
+- Phone number is a mandatory field for the phone type. Starting at +380
+- Email - email must have @ and a dot
+- After sending, we display the data entered by the user in the console.
+
+When an error occurs, show it below the field.
---
+++ lesson_13/lesson_13.1/script.js
@@ -0,0 +1,78 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("#myForm");
+
+    // input for name
+    const blockForName = form.querySelector("#blockForName");
+    const name = blockForName.querySelector("#name");
+    const masageForName = blockForName.querySelector("#masageForName");
+
+    name.addEventListener('input', () => {
+        if (name.value.trim() === "") {
+            masageForName.textContent = "Введіть своє ім'я.";
+        } else {
+            masageForName.textContent = "";
+        }
+    })
+
+    // input for message
+    const blockForMessage = form.querySelector("#blockForMessage");
+    const message = blockForMessage.querySelector("#message");
+    const messageForMessage = blockForMessage.querySelector("#messageForMessage");
+
+    message.addEventListener("input", () => {
+        if (message.value.trim().length < 5) {
+            messageForMessage.textContent = "Введіть неменьше 5 символів в текстове поле.";
+        } else {
+            messageForMessage.textContent = "";
+        }
+    })
+
+    // input for number
+    const blockForNumber = form.querySelector("#blockForNumber");
+    const phoneNumber = blockForNumber.querySelector("#phone_number");
+    const messageForNumber = blockForNumber.querySelector("#messageForNumber");
+
+    phoneNumber.addEventListener('input', () => {
+        if (!phoneNumber.value.startsWith("+380")) {
+            messageForNumber.textContent = "Допишіть до початка '+380' ";
+        } else if (phoneNumber.value.length !== 13) {
+            messageForNumber.textContent = "Номер повинен мати 13 символів, провірьте кількість символів.";
+        } else {
+            messageForNumber.textContent = "";
+        }
+    })
+
+    // input for email
+    const blockForEmail = form.querySelector("#blockForEmail");
+    const email = blockForEmail.querySelector("#email");
+    const messageForEmail = blockForEmail.querySelector("#messageForEmail");
+
+    email.addEventListener('input', () => {
+        if (!email.value.includes("@")) {
+            messageForEmail.textContent = "Email повинен містити '@'.";
+            messageForEmail.style.color = "red";
+        } else if (!email.value.includes(".")) {
+            messageForEmail.textContent = "Email повинен містити '.'.";
+            messageForEmail.style.color = "red";
+        } else {
+            messageForEmail.textContent = "";
+        };
+    });
+    form.addEventListener('submit', (event) => {
+        // не дает перезагрузить страницу
+        event.preventDefault();
+
+        // 1 variant
+        console.log("Name:", name.value);
+        console.log("Message:", message.value);
+        console.log("Phone Number:", phoneNumber.value);
+        console.log("Email:", email.value);
+
+        // 2 variant
+        console.log(`Пользователь ввел: 
+            Name: ${name.value},
+            Message: ${message.value},
+            Phone Number: ${phoneNumber.value},
+            Email: ${email.value}`)
+    });
+});
