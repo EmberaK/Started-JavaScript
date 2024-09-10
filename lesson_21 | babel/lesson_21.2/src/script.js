8cddad58e8f7308e9851f7d8b80cd137418dabf5
EmberaK <vlasukula11@gmail.com>
2024-09-10 19:10:31 +0300

    added solution for lesson_21 about babel

---
+++ lesson_21 | babel/lesson_21.2/src/script.js
@@ -0,0 +1,9 @@
+var map = L.map('map').setView([50.4501, 30.5234], 13);  // Coordinates of Kyiv
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var marker = L.marker([50.4501, 30.5234]).addTo(map);
+
+marker.bindPopup("<b>Kyiv</b><br>The capital of Ukraine.<br>").openPopup();
